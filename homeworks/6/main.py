419b5dd1c7f38f7e9234ceadc1709f0b96fa5ba6
tresabhi <43380238+tresabhi@users.noreply.github.com>
2025-05-01 13:55:30 -0500

    gamma plot

--- homeworks/6/main.py
+++ homeworks/6/main.py
@@ -11,7 +11,7 @@ from tabulate import tabulate
 
 N = 150  # sections
 epsilon = 1e-8  # threshold
-beta = 1e-2  # damping
+beta = 10**-2.5  # damping
 
 V_infinity = 65
 
@@ -22,18 +22,28 @@ b = math.sqrt(S * AR)
 c_r = 2 * S / (b * (1 + lambda_))
 c_t = lambda_ * c_r
 
-alpha_0_deg = -17
-alpha_1_deg = 17
-d_alpha_deg = 1
-alphas_deg = np.linspace(
-    alpha_0_deg, alpha_1_deg, int((alpha_1_deg - alpha_0_deg) / d_alpha_deg) + 1
+lift_plot_alpha_0_deg = -17
+lift_plot_alpha_1_deg = 17
+lift_plot_d_alpha_deg = 1
+lift_plot_alphas_deg = np.linspace(
+    lift_plot_alpha_0_deg,
+    lift_plot_alpha_1_deg,
+    int((lift_plot_alpha_1_deg - lift_plot_alpha_0_deg) / lift_plot_d_alpha_deg) + 1,
 )
-alphas = np.deg2rad(alphas_deg)
 
-alpha_linear_0_deg = -8
-alpha_linear_1_deg = 8
-alpha_linear_0 = alpha_linear_0_deg * math.pi / 180
-alpha_linear_1 = alpha_linear_1_deg * math.pi / 180
+slope_alpha_0_deg = -8
+slope_alpha_1_deg = 8
+slope_alpha_0 = slope_alpha_0_deg * math.pi / 180
+slope_alpha_1 = slope_alpha_1_deg * math.pi / 180
+
+gamma_plot_alpha_0_deg = -20
+gamma_plot_alpha_1_deg = 20
+gamma_plot_d_alpha_deg = 5
+gamma_plot_alphas_deg = np.linspace(
+    gamma_plot_alpha_0_deg,
+    gamma_plot_alpha_1_deg,
+    int((gamma_plot_alpha_1_deg - gamma_plot_alpha_0_deg) / gamma_plot_d_alpha_deg) + 1,
+)
 
 # incidence angles at root and tip
 i_r = 1.5 * math.pi / 180
@@ -107,15 +117,19 @@ c_l_z_0 = c_l(incidence(0))  # c_l of the incidence angle at z = 0
 Gamma_0 = V_infinity * c_r / 2 * c_l_z_0
 
 
+def z(i):
+    return -b / 2 + delta_z / 2 + (i - 1) * delta_z
+
+
 # i just love the word ephemeral; here, it means initial
 def Gamma_ephemeral(i):
-    z_i = -b / 2 + delta_z / 2 + (i - 1) * delta_z
-    return Gamma_0 * math.sqrt(1 - (z_i / (b / 2)) ** 2)
+    return Gamma_0 * math.sqrt(1 - (z(i) / (b / 2)) ** 2)
 
 
-def converge(alpha):
-    print(f"🟡 Received request for alpha = {round(alpha * 180 / math.pi)}°")
+def converge_Gammas(alpha_deg):
+    print(f"🟡 Received request for alpha = {alpha_deg}°")
 
+    alpha = alpha_deg * math.pi / 180
     Gammas = [Gamma_ephemeral(i) for i in range(1, N + 1)]
     converged = False
     iteration = 0
@@ -139,44 +153,10 @@ def converge(alpha):
         f"🟢 Converged alpha = {round(alpha * 180 / math.pi)}° in {iteration} iterations"
     )
 
-    C_L = 2 * delta_z / (V_infinity * S) * sum(Gammas[i - 1] for i in range(1, N + 1))
-    C_D_i = (
-        -2
-        * delta_z
-        / (V_infinity * S)
-        * sum(Gammas[i - 1] * math.sin(alpha_i_i(Gammas, i)) for i in range(1, N + 1))
-    )
-
-    return (C_L, C_D_i)
+    return Gammas
 
 
-def main():
-    C_Ls = []
-    C_D_is = []
-
-    # I threaded this because I am kinda tired of watching paint dry
-    print(f"🔵 Spawning {os.cpu_count()} asynchronous compute threads...")
-    with concurrent.futures.ProcessPoolExecutor() as executor:
-        results = executor.map(converge, alphas)
-
-    for C_L, C_D_i in results:
-        C_Ls.append(C_L)
-        C_D_is.append(C_D_i)
-
-    c_l_0 = c_l(alpha_linear_0)
-    c_l_1 = c_l(alpha_linear_1)
-    C_L_0 = C_Ls[np.where(alpha_linear_0_deg == alphas_deg)[0][0]]
-    C_L_1 = C_Ls[np.where(alpha_linear_1_deg == alphas_deg)[0][0]]
-
-    a_0 = (c_l_1 - c_l_0) / (alpha_linear_1 - alpha_linear_0)
-    a = (C_L_1 - C_L_0) / (alpha_linear_1 - alpha_linear_0)
-
-    print(
-        tabulate(
-            [["a_0 (airfoil slope)", a_0], ["a (3D wing slope)", a]],
-        )
-    )
-
+def render_coefficients(C_Ls, C_D_is):
     _, ax1 = plt.subplots()
 
     ax1.plot(
@@ -188,12 +168,12 @@ def main():
     ax1.legend(loc="upper left")
 
     ax2 = ax1.twinx()
-    ax2.plot(alphas_deg, C_D_is, color="tab:red", label="C_D_i")
+    ax2.plot(lift_plot_alphas_deg, C_D_is, color="tab:red", label="C_D_i")
     ax2.set_ylabel("C_D_i", color="tab:red")
     ax2.tick_params(axis="y", labelcolor="tab:red")
     ax2.legend(loc="lower right")
 
-    ax1.plot(alphas_deg, C_Ls, color="tab:blue", label="C_L")
+    ax1.plot(lift_plot_alphas_deg, C_Ls, color="tab:blue", label="C_L")
     ax1.set_xlabel("Alpha (degrees)")
     ax1.set_ylabel("C_L", color="tab:blue")
     ax1.tick_params(axis="y", labelcolor="tab:blue")
@@ -201,6 +181,80 @@ def main():
 
     plt.title("C_L and C_D_i vs Alpha")
     plt.show()
+
+
+def render_Gammas():
+    print(Gamma_cache)
+    alphas_deg = np.linspace(
+        gamma_plot_alpha_0_deg, gamma_plot_alpha_1_deg, gamma_plot_d_alpha_deg
+    )
+    zs = [z(i) for i in range(1, N + 1)]
+
+    _, ax = plt.subplots()
+
+    for alpha_deg in gamma_plot_alphas_deg:
+        Gammas = Gamma_cache[alpha_deg] / Gamma_0
+        ax.plot(zs, Gammas, label=f"{alpha_deg}°")
+
+    plt.title("Gamma vs Z")
+    plt.show()
+
+
+# i made a cache because i need gammas again for the second plot
+Gamma_cache = {}
+
+
+def cache_Gammas():
+    alphas_deg = list(set([*lift_plot_alphas_deg, *gamma_plot_alphas_deg]))
+
+    print(
+        f"🔵 Spawning {os.cpu_count()} asynchronous compute threads for {len(alphas_deg)} Gammas..."
+    )
+    with concurrent.futures.ProcessPoolExecutor() as executor:
+        Gammas_array = executor.map(converge_Gammas, alphas_deg)
+
+    for alpha_deg, Gammas in zip(alphas_deg, Gammas_array):
+        Gamma_cache[alpha_deg] = Gammas
+
+
+def main():
+    cache_Gammas()
+    render_Gammas()
+
+    C_Ls = []
+    C_D_is = []
+
+    for alpha_deg in lift_plot_alphas_deg:
+        Gammas = Gamma_cache[alpha_deg]
+        C_L = (
+            2 * delta_z / (V_infinity * S) * sum(Gammas[i - 1] for i in range(1, N + 1))
+        )
+        C_D_i = (
+            -2
+            * delta_z
+            / (V_infinity * S)
+            * sum(
+                Gammas[i - 1] * math.sin(alpha_i_i(Gammas, i)) for i in range(1, N + 1)
+            )
+        )
+        C_Ls.append(C_L)
+        C_D_is.append(C_D_i)
+
+    c_l_0 = c_l(slope_alpha_0)
+    c_l_1 = c_l(slope_alpha_1)
+    C_L_0 = C_Ls[np.where(slope_alpha_0_deg == lift_plot_alphas_deg)[0][0]]
+    C_L_1 = C_Ls[np.where(slope_alpha_1_deg == lift_plot_alphas_deg)[0][0]]
+
+    a_0 = (c_l_1 - c_l_0) / (slope_alpha_1 - slope_alpha_0)
+    a = (C_L_1 - C_L_0) / (slope_alpha_1 - slope_alpha_0)
+
+    print(
+        tabulate(
+            [["a_0 (airfoil slope)", a_0], ["a (3D wing slope)", a]],
+        )
+    )
+
+    render_coefficients(C_Ls, C_D_is)
 
 
 if __name__ == "__main__":
